461b7de0651d29955959b6f7103a44b68713a059
TianHero <1246127205@qq.com>
2016-05-10 21:25:56 +0800

    ZT---弹出菜单

--- 吃货美食/吃货美食/Class/EChat/Controller/CHEChatViewController.m
+++ 吃货美食/吃货美食/Class/EChat/Controller/CHEChatViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "CHEChatViewController.h"
-
+#import "PellTableViewSelect.h"
 @interface CHEChatViewController ()
 
 @end
@@ -16,7 +16,27 @@
 
 -(void)loadView
 {
-    self.navigationItem.rightBarButtonItem = [UIBarButtonItem barItemWithImageName:@"topsearchicon_grey_2" withSelectImage:nil withHorizontalAlignment:UIControlContentHorizontalAlignmentRight withTarget:self action:@selector(search) forControlEvents:UIControlEventTouchUpInside];
+    //leftBarButtonItem
+    UIBarButtonItem *leftItemAdd = [UIBarButtonItem barItemWithImageName:@"umeng_add_photo" withSelectImage:nil withHorizontalAlignment:UIControlContentHorizontalAlignmentLeft withTarget:self action:@selector(addClick) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = leftItemAdd;
+    
+    //rightBarButtonItems
+    UIBarButtonItem *rightItemMail = [UIBarButtonItem barItemWithImageName:@"ms_mail2" withSelectImage:nil withHorizontalAlignment:UIControlContentHorizontalAlignmentRight withTarget:self action:@selector(msMail) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIBarButtonItem *rightItemSearch = [UIBarButtonItem barItemWithImageName:@"topsearchicon_grey_2" withSelectImage:nil withHorizontalAlignment:UIControlContentHorizontalAlignmentRight withTarget:self action:@selector(search) forControlEvents:UIControlEventTouchUpInside];
+    
+    self.navigationItem.rightBarButtonItems = @[rightItemSearch,rightItemMail];
+}
+- (void)msMail
+{
+    
+}
+- (void)addClick
+{
+    // 弹出QQ的自定义视图
+    [PellTableViewSelect addPellTableViewSelectWithWindowFrame:CGRectMake(self.view.bounds.size.width - 100, 64, 150, 200) selectData:@[@"发食话",@"餐桌食光",@"玩转烘焙",@"美食讲堂"] images:@[@"saoyisao.png",@"jiahaoyou.png",@"taolun.png",@"diannao.png"] action:^(NSInteger index) {
+        NSLog(@"选择%ld",index);
+    } animated:YES];
 }
 - (void)search
 {
