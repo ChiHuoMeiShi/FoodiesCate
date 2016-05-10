db1cbceedbb2f07899251383e8dbedc5e6f8ed8c
1457792186 <1457792186@qq.com>
2016-05-10 09:18:17 +0800

    JW：HomeVC get data

--- 吃货美食/吃货美食/Class/Recommend/Controller/CHRecommendViewController.m
+++ 吃货美食/吃货美食/Class/Recommend/Controller/CHRecommendViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CHRecommendViewController.h"
+#import <AFNetworking.h>
 
 @interface CHRecommendViewController ()
 
@@ -16,7 +17,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    AFHTTPSessionManager * manger = [AFHTTPSessionManager manager];
+    manger.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html", nil];
+    
+    NSString * url = @"http://api.meishi.cc/v5/index5.php?format=json";
+    
+    NSDictionary * dic = @{@"lat":@"34.6049907522264",@"lon":@"112.4229875834745",@"source":@"iphone",@"format":@"json",@"page":@"1",@"app_liketime":@"1462495842"};
+    [manger POST:url parameters:dic progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        CHLog(@"%@",responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        CHLog(@"%@",error);
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
