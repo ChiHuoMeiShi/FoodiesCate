0b0b2fe699fb1cee1a8e749946687d468074185c
1457792186 <1457792186@qq.com>
2016-05-12 20:07:41 +0800

    JW：fenlei list plist

--- 吃货美食/吃货美食/Class/Recommend/Controller/CHRecommendViewController.m
+++ 吃货美食/吃货美食/Class/Recommend/Controller/CHRecommendViewController.m
@@ -62,7 +62,7 @@
 - (void)getCollectionViewData{
     AFHTTPSessionManager * manger = [AFHTTPSessionManager manager];
     manger.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html", nil];
-    
+
     NSString * url = @"http://api.meishi.cc/v5/index5.php?format=json";
     NSDictionary * dic = @{@"lat":@"34.6049907522264",@"lon":@"112.4229875834745",@"source":@"iphone",@"format":@"json",@"page":@"1",@"app_liketime":@"1462495842"};
     [manger POST:url parameters:dic progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
