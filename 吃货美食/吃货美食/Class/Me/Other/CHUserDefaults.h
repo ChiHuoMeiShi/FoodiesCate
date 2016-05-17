385d1023d6a83f542c99b431778078c12305e709
TianHero <1246127205@qq.com>
2016-05-17 16:13:25 +0800

    ZT:用户信息保存到本地

---
+++ 吃货美食/吃货美食/Class/Me/Other/CHUserDefaults.h
@@ -0,0 +1,27 @@
+//
+//  CHUserDefaults.h
+//  吃货美食
+//
+//  Created by 赵天 on 16/5/17.
+//  Copyright © 2016年 赵天. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHUserDefaults : NSObject
+
+@property (nonatomic,strong) NSNumber * code;
+
+@property (nonatomic,copy) NSString * photo;
+
+@property (nonatomic,copy) NSString * sex;
+
+@property (nonatomic,copy) NSString * user_id;
+
+@property (nonatomic,copy) NSString * user_name;
+
++ (instancetype)shareUserDefault;
+
+- (void)setUserDict:(NSDictionary *)userDict;
+
+@end
---
+++ 吃货美食/吃货美食/Class/Me/Other/CHUserDefaults.m
@@ -0,0 +1,48 @@
+//
+//  CHUserDefaults.m
+//  吃货美食
+//
+//  Created by 赵天 on 16/5/17.
+//  Copyright © 2016年 赵天. All rights reserved.
+//
+
+#import "CHUserDefaults.h"
+#define kUserDict @"userDict"
+static CHUserDefaults *userDefault = nil;
+@implementation CHUserDefaults
+
++ (instancetype)shareUserDefault
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        userDefault = [[CHUserDefaults alloc] init];
+        [userDefault loadUserInfo];
+    });
+    return userDefault;
+}
+- (void)loadUserInfo
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    //取到本地的userDict
+    NSDictionary *userDict = [defaults objectForKey:kUserDict];
+    if (userDict)
+    {
+        self.user_id = [userDict objectForKey:@"user_id"];
+        self.user_name = [userDict objectForKey:@"user_name"];
+        self.sex = [userDict objectForKey:@"sex"];
+        self.photo = [userDict objectForKey:@"photo"];
+    }
+}
+- (void)setUserDict:(NSDictionary *)userDict
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    //把传进来的userDict保留到本地
+    [defaults setObject:userDict forKey:kUserDict];
+    self.user_id = [userDict objectForKey:@"user_id"];
+    self.user_name = [userDict objectForKey:@"user_name"];
+    self.sex = [userDict objectForKey:@"sex"];
+    self.photo = [userDict objectForKey:@"photo"];
+}
+
+@end
